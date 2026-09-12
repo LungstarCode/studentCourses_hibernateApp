2af26d791e2cb02d282ac941353f8f8e44b593b9
LungstarCode <lungilemawila5@gmail.com>
2026-09-12 21:40:21 +0200

    added a fully implemented Student Class

--- CoursesApp/src/main/java/com/courses/models/Student.java
+++ CoursesApp/src/main/java/com/courses/models/Student.java
@@ -1,5 +1,59 @@
 package com.courses.models;
 
-public class Student {
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="students")
+public class Student {
+	
+	@Id
+	private Integer student_id;
+	private String student_name;
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	private Set<Course> courses;
+
+	public Integer getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(Integer student_id) {
+		this.student_id = student_id;
+	}
+
+	public String getStudent_name() {
+		return student_name;
+	}
+
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+
+	public Set<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+	
+	public Student()
+	{
+		System.out.println("No-arguement constructor for hibernate");
+	}
+
+	@Override
+	public String toString() {
+		return "Student [student_id=" + student_id + ", student_name=" + student_name + ", courses=" + courses + "]";
+	}
+	
+	
+	
+	
 }
