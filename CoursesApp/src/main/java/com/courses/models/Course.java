3ac75fff430a637425b7968bc4404fe5ac757a49
LungstarCode <lungilemawila5@gmail.com>
2026-09-12 21:32:07 +0200

    added the instance variables in the Course class, including the Student instance Object reference with the ManyToMany association

--- CoursesApp/src/main/java/com/courses/models/Course.java
+++ CoursesApp/src/main/java/com/courses/models/Course.java
@@ -1,5 +1,58 @@
 package com.courses.models;
 
-public class Course {
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="courses")
+public class Course {
+	
+	@Id
+	private Integer course_id;
+	
+	private String course_name;
+	
+	private Integer price;
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	private Set<Student> student;
+
+	public Integer getCourse_id() {
+		return course_id;
+	}
+
+	public void setCourse_id(Integer course_id) {
+		this.course_id = course_id;
+	}
+
+	public String getCourse_name() {
+		return course_name;
+	}
+
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Set<Student> getStudent() {
+		return student;
+	}
+
+	public void setStudent(Set<Student> student) {
+		this.student = student;
+	}
+
+	
 }
